4067f5e0d74af2993acb90ffe6ae17029ff4937d
YasmineEssaid <YasmineEssaid@YASMINE-ESSAID>
2024-11-24 18:21:57 +0100

    commit

--- inventory-service/src/main/java/org/xproce/inventoryservice/repositories/CreatorRepository.java
+++
@@ -1,7 +0,0 @@
-package org.xproce.inventoryservice.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.xproce.inventoryservice.entities.Creator;
-
-public interface CreatorRepository extends JpaRepository<Creator, Long> {
-}
